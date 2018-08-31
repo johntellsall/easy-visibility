ab7970267c18f612fe6566f44ac8af6d29444fc3
John Mitchell <jmitchell@tuition.io>
2018-08-31 10:52:47 -0700

    wip

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # easy-visibility
 Production should be obvious, with changes immediately being visible.
+
+
+# Pingdom
+
+## Terraform plugin
+
+- https://github.com/russellcardullo/terraform-provider-pingdom
+
--- pingdom.tf
+++ pingdom.tf
@@ -1,20 +1,20 @@
-# variable "pingdom_user" {}
-# variable "pingdom_password" {}
+variable "pingdom_user" {}
+variable "pingdom_password" {}
 variable "pingdom_api_key" {}
 
 # variable "pingdom_account_email" {} # Optional: only required for multi-user accounts
 
 provider "pingdom" {
-  # user          = "${var.pingdom_user}"
-  # password      = "${var.pingdom_password}"
-  api_key = "${var.pingdom_api_key}"
+  user     = "${var.pingdom_user}"
+  password = "${var.pingdom_password}"
+  api_key  = "${var.pingdom_api_key}"
 
   # account_email = "${var.pingdom_account_email}" # Optional: only required for multi-user accounts
 }
 
 resource "pingdom_check" "example" {
   type       = "http"
-  name       = "my http check"
+  name       = "my http check 1015"
   host       = "example.com"
   resolution = 5
 }
@@ -26,15 +26,22 @@ resource "pingdom_check" "example" {
 #   resolution               = 5
 #   sendnotificationwhendown = 2
 
+
 #   integrationids = [
 #     12345678,
 #     23456789,
 #   ]
 # }
 
-resource "pingdom_check" "ping_example" {
-  type       = "ping"
-  name       = "my ping check"
-  host       = "example.com"
-  resolution = 1
-}
+
+# * pingdom_check.ping_example: 500 Internal Server Error: Internal Server Error
+# resource "pingdom_check" "ping_example" {
+#   type       = "ping"
+#   name       = "my ping check"
+#   host       = "example.com"
+#   resolution = 5
+
+
+#   sendnotificationwhendown = 2
+# }
+
